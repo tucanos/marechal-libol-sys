f405fcb4212c8810428f42170d1e2e72a295372a
Jerome Robert <jerome.robert@gmx.com>
2023-02-24 10:28:38 +0100

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+# About
+
+This is a [bindgen](https://github.com/rust-lang/rust-bindgen) wrapper of [Loic Marechal's libOL](https://github.com/LoicMarechal/libOL.git).
+
+# Using
+
+If libOL.1 is not in the standard location you should define required environment variables in
+[`.cargo/config.toml`](https://doc.rust-lang.org/cargo/reference/config.html#env) (or defined them in any other way):
+
+```toml
+[env]
+LIBOL_INLCUDE_DIR="/path/to/libol-install/include"
+# or
+LIBOL_DIR="path/to/libol-install"
+# optionally
+LIBOL_LIB_DIR="/path/to/lib"
+```
+
+Then
+
+```
+cargo add --git https://github.com/jeromerobert/marechal-libol-sys.git
+```
---
+++ build.rs
@@ -0,0 +1,50 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let include_dir = match env::var("LIBOL_INCLUDE_DIR") {
+        Ok(x) => PathBuf::from(x),
+        Err(_) => match env::var("LIBOL_DIR") {
+            Ok(x) => PathBuf::from(x).join("include"),
+            Err(_) => PathBuf::from("/usr/include"),
+        },
+    };
+    let libol_h = include_dir
+        .join("libol1.h")
+        .into_os_string()
+        .into_string()
+        .expect("Not an UTF-8 path");
+    let lib_dir = match env::var("LIBOL_LIB_DIR") {
+        Ok(x) => Some(PathBuf::from(x)),
+        Err(_) => env::var("LIBOL_DIR")
+            .map(|x| PathBuf::from(x).join("lib"))
+            .ok(),
+    };
+    if let Some(lib_dir) = lib_dir {
+        println!(
+            "cargo:rustc-link-search={}",
+            lib_dir
+                .into_os_string()
+                .into_string()
+                .expect("Not an UTF-8 path")
+        );
+    }
+    println!("cargo:rerun-if-changed={}", libol_h);
+    println!("cargo:rustc-link-lib=OL.1");
+    bindgen::Builder::default()
+        .header(libol_h)
+        .allowlist_type("TypTag")
+        .rustified_enum("TypTag")
+        .allowlist_function("Lol.*")
+        .clang_args(
+            env::var("LIBOL_CFLAGS")
+                .unwrap_or_default()
+                .split_whitespace(),
+        )
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("src/binding.rs")
+        .expect("Couldn't write bindings!");
+}
